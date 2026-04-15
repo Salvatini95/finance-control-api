61d7a445d71f3da72869c68dc148137465c6531d
Guiherme Salvatini <salvatiniguilherme@gmail.com>
2026-04-15 13:49:41 -0300

    Update test_email.py

--- test_email.py
+++ test_email.py
@@ -4,7 +4,7 @@ os.environ["RESEND_API_KEY"] = "re_WFgXh2fs_Aqo7tFquscU1DksPEWXoAwYf"
 from app.email_service import send_verification_email
 
 result = send_verification_email(
-    to_email="seu@email.com",
+    to_email="salvatiniguilherme@gmail.com",
     name="Guilherme",
     token="token-de-teste-123"
 )
