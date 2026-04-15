df188be9e063d49c9f7c028de6477fbbc0d75188
Guiherme Salvatini <salvatiniguilherme@gmail.com>
2026-04-15 10:38:50 -0300

    Rename test_bp to dev_bp for development routes

--- app/routes/dev_routes.py
+++ app/routes/dev_routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify
 from app.email_service import send_verification_email
 
-test_bp = Blueprint("test", __name__)
+dev_bp = Blueprint("dev", __name__)
 
-@test_bp.route("/test-email", methods=["GET"])
+@dev_bp.route("/dev/test-email", methods=["GET"])
 def test_email():
     ok = send_verification_email(
         to_email="salvatiniguilherme@gmail.com",
